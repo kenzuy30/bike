f9cbccdccfed8c4d6ffb3eed580f9c0f0059b65a
Kent Jasper P. Bagumbayan <145415133+kenzuy30@users.noreply.github.com>
2024-01-19 22:32:16 +0800

    config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php 
     try {
         $host = "localhost";
-        $dbname = "id21790251_bikes";
-        $user = "id21790251_bike";
-        $password = "Qwerty123!";
+        $dbname = "system";
+        $user = "root";
+        $password = "";
 
         $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
